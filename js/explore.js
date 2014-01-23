6f27372ff29557cc319b092081f94507143c74ab
Jonas Wiklund <jonas.wiklund@atex.com>
2014-01-23 11:27:57 +0100

    All but hangers done

--- js/explore.js
+++ js/explore.js
@@ -9,39 +9,94 @@ var explore = function(window, $) {
       username: details.user,
       password: details.pass
     }
-    function changeDetect() {
-      var nid = idelement.val();
-      if (nid != id) {
-        id = nid;
-        updateSearch(id);
+    function finalResult(validForId, data) {
+      if (validForId != id) {
+        return;
       }
+      $('#exploreresult').text(JSON.stringify(data, null, "  "));
+      $('#explorefound').css('display', 'block');
+    }
+    function expandResult(validForId, dataType, topData, currentData, resultKey, newData) {
+      if (validForId != id) {
+        return;
+      }
+      if (resultKey) {
+        currentData[resultKey] = newData;
+      } else {
+        topData = newData;
+      }
+      if (dataType == 'HangerId') {
+        $.ajax(base + 'HangerVersion::' + newData.key, $.extend({}, settings, {
+          success: expandResult.bind(expandResult, validForId, 'HangerVersion', topData, newData, 'key :  ' + newData.key + ' (HangerVersion)'),
+          error: expandError.bind(expandError, validForId, topData, newData, 'key-' + newData.key)
+        }));
+      } else if (dataType == 'HangerVersion') {
+        $.ajax(base + 'Hanger::' + newData.contentId.key + "::" + newData.version, $.extend({}, settings, {
+          success: expandResult.bind(expandResult, validForId, 'Hanger', topData, newData, 'version :  ' + newData.version + ' (Hanger)'),
+          error: expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version)
+        }));
+      } else if (dataType == 'Hanger') {
+        // TODO expand all aspects
+        finalResult(validForId, topData);
+      } else if (dataType == 'AspectVersion') {
+        $.ajax(base + 'Aspect::' + newData.contentId.key + '::' + newData.version, $.extend({}, settings, {
+          success: expandResult.bind(expandResult, validForId, 'Aspect', topData, newData, 'version :  ' + newData.version + ' (Aspect'),
+          error: expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version)
+        }));
+      } else {
+        finalResult(validForId, topData);
+      }
+    }
+    function expandError(validForId, topData, currentData, resultKey, jqXHR, textStatus) {
+      if (validForId != id) {
+        return;
+      }
+      currentData[resultKey] = 'not found';
+      finalResult(validForId, topData);
+    }
+    function typeFromId(id) {
+      if (id.indexOf('::') == -1) {
+        return 'unknown';
+      }
+      return id.substring(0, id.indexOf('::'));
     }
     function updateSearch(searchFor) {
       $.ajax(base + searchFor, $.extend({}, settings, {
-        success: function(data) {
-          if (searchFor == id) {
-            $('#exploreresult').text(JSON.stringify(data, null, "  "));
-            $('#explorefound').css('display', 'block');
-          }
-        },
+        success: expandResult.bind(expandResult, searchFor, typeFromId(searchFor), null, null, null),
         error: function(jqXHR) {
           if (searchFor == id) {
             $.ajax(base + 'HangerId::' + searchFor, $.extend({}, settings, {
-              success: function(data) {
-                if (searchFor == id) {
-                  $('#exploreresult').text(JSON.stringify(data, null, "  "));
-                  $('#explorefound').css('display', 'block');
-                }
-              },
+              success: expandResult.bind(expandResult, searchFor, 'HangerId', null, null, null),
               error: function(jqXHR) {
                 if (searchFor == id) {
-                  $('#explorefound').css('display', 'none');
+                  $.ajax(base + 'HangerVersion::' + searchFor, $.extend({}, settings, {
+                    success: expandResult.bind(expandResult, searchFor, 'HangerVersion', null, null, null),
+                    error: function(jqXHR) {
+                      if (searchFor == id) {
+                        $.ajax(base + 'AspectVersion::' + searchFor, $.extend({}, settings, {
+                          success: expandResult.bind(expandResult, searchFor, 'AspectVersion', null, null, null),
+                          error: function(jqXHR) {
+                            if (searchFor == id) {
+                              $('#explorefound').css('display', 'none');
+                            }
+                          }
+                        }));
+                      }
+                    }
+                  }));
                 }            
               }
             }));
           }
         }
       }));
+    }
+    function changeDetect() {
+      var nid = idelement.val();
+      if (nid != id) {
+        id = nid;
+        updateSearch(id);
+      }
     }
     var idelement = $('#exploreid');
     idelement.keyup(changeDetect);
