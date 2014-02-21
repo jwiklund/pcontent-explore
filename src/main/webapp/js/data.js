d64949b0b6fbbdeb7274238804c5c58514dce30b
Jonas Wiklund <jonas.wiklund@atex.com>
2014-02-21 14:04:40 +0100

    Removed JSLint warnings

--- src/main/webapp/js/data.js
+++ src/main/webapp/js/data.js
@@ -1,147 +1,142 @@
-function data(window, $) {
-  var base = 'da/', id = '', variant = '', aspect = '', pcontent = '', settings = {
-    cache: false,
-    dataType: 'json'
-  };
-  $('#exploreresult').on('dblclick', 'pre', function(e) {
-    var pre = $('#exploreresult pre');
-    if (!pre.attr('contenteditable')) {
-      var data = JSON.parse(pre.text());
-      delete data['id'];
-      delete data['version'];
-      delete data['meta'];
-      $('#exploreresult').html('<pre contenteditable="true"><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
-      $('#exploreresult pre code').each(function(i, e) { hljs.highlightBlock(e); });
-      $('#explorefound').css('display', 'none');
-      $('#editsave').css('display', 'block');
-    }
-  });
-  $('#plink').on('click', function(e) {
-    document.location = constructurl('index.html', '#', id, variant, aspect, pcontent);
-    e.preventDefault();
-  });
-  function constructurl(url, quest, id, variant, aspect, pcontent) {
-    if (id) {
-      url = url + quest + 'id=' + id;
-      quest = '&';
-    }
-    if (variant) {
-      url = url + quest + 'variant=' + variant;
-      quest = '&';
-    }
-    if (aspect) {
-      url = url + quest + 'aspect=' + aspect;
-    }
-    if (pcontent) {
-      url = url + quest + 'key=' + pcontent;
-    }
-    return url;
-  }
-  $('#editsave').on('click', function(e) {
-    var data = $('#exploreresult pre').text(),
-      url = constructurl(base, '?', id, variant, aspect);
-    $.ajax(url, $.extend({}, { data: data, contentType: 'application/json', method: 'PUT' }))
-      .done(function(result) {
-        if (result.id) {
-          id = result.id;
-          $('#exploreid').val(id);
+function data(window, $, hljs) {
+    "use strict";
+    var base = 'da/', id = '', variant = '', aspect = '', pcontent = '',
+        idelement = $('#exploreid'), variantelement = $('#explorevariant'), aspectelement = $('#exploreaspect'),
+        hash, settings = { cache: false, dataType: 'json' };
+    function constructurl(url, quest, id, variant, aspect, pcontent) {
+        if (id) {
+            url = url + quest + 'id=' + id;
+            quest = '&';
         }
-        finalResult(id, variant, aspect, result);
-      });
-  });
-  function finalResult(validForId, validForVariant, validForAspect, data) {
-    if (validForId != id || validForVariant != variant || validForAspect != aspect) {
-      return;
+        if (variant) {
+            url = url + quest + 'variant=' + variant;
+            quest = '&';
+        }
+        if (aspect) {
+            url = url + quest + 'aspect=' + aspect;
+        }
+        if (pcontent) {
+            url = url + quest + 'key=' + pcontent;
+        }
+        return url;
     }
-    if (data.statusCode) {
-      $('#exploreerror').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
-      $('#exploreerror pre code').each(function(i, e) { hljs.highlightBlock(e); });
-      $('#exploreerror').css('display', 'block');
-      return;
+    function finalResult(validForId, validForVariant, validForAspect, data) {
+        if (validForId !== id || validForVariant !== variant || validForAspect !== aspect) {
+            return;
+        }
+        if (data.statusCode) {
+            $('#exploreerror').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
+            $('#exploreerror pre code').each(function (i, e) { hljs.highlightBlock(e); });
+            $('#exploreerror').css('display', 'block');
+            return;
+        }
+        $('#exploreerror').css('display', 'none');
+        $('#exploreresult').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
+        $('#exploreresult pre code').each(function (i, e) { hljs.highlightBlock(e); });
+        $('#explorefound').css('display', 'block');
+        $('#editsave').css('display', 'none');
+        var url = window.document.location.href;
+        if (url.indexOf('#') !== -1) {
+            url = url.substring(0, url.indexOf('#'));
+        }
+        url = url + '#id=' + validForId;
+        if (validForVariant) {
+            url = url + '&variant=' + validForVariant;
+        }
+        if (validForAspect) {
+            url = url + '&aspect=' + validForAspect;
+        }
+        window.document.location = url;
     }
-    $('#exploreerror').css('display', 'none');
-    $('#exploreresult').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
-    $('#exploreresult pre code').each(function(i, e) { hljs.highlightBlock(e); });
-    $('#explorefound').css('display', 'block');
-    $('#editsave').css('display', 'none');
-    var url = document.location.href;
-    if (url.indexOf('#') != -1) {
-      url = url.substring(0, url.indexOf('#'));
+    function updateSearch(validForId, validForVariant, validForAspect) {
+        if (validForId !== id || validForVariant !== variant || validForAspect !== aspect) {
+            return;
+        }
+        $('#explorefound').css('display', 'none');
+        if (validForId === '') {
+            return;
+        }
+        var url = constructurl(base, '?', validForId, validForVariant, validForAspect);
+        $.ajax(url, settings)
+            .done(finalResult.bind(finalResult, validForId, validForVariant, validForAspect));
     }
-    url = url + '#id=' + validForId;
-    if (validForVariant) {
-      url = url + '&variant=' + validForVariant;
+    function changeDetect() {
+        var nid = idelement.val(), nvariant = variantelement.val(), naspect = aspectelement.val(), changed = false;
+        if (nid !== id) {
+            id = nid;
+            changed = true;
+        }
+        if (nvariant !== variant) {
+            variant = nvariant;
+            changed = true;
+        }
+        if (naspect !== aspect) {
+            aspect = naspect;
+            changed = true;
+        }
+        if (changed) {
+            updateSearch(id, variant, aspect);
+        }
     }
-    if (validForAspect) {
-      url = url + '&aspect=' + validForAspect;
+    function hashKey() {
+        var hash = window.document.location.hash;
+        function parseHash(name) {
+            var ind = hash.indexOf(name + '='), end;
+            if (ind === -1) {
+                return '';
+            }
+            end = hash.indexOf('&', ind + name.length + 1);
+            if (end === -1) {
+                end = hash.length;
+            }
+            return hash.substring(ind + name.length + 1, end);
+        }
+        return {
+            id: parseHash('id'),
+            variant: parseHash('variant'),
+            aspect: parseHash('aspect'),
+            pcontent: parseHash('key')
+        };
     }
-    document.location = url;
-  }
-  function updateSearch(validForId, validForVariant, validForAspect) {
-    if (validForId != id || validForVariant != variant || validForAspect != aspect) {
-      return;
-    }
-    $('#explorefound').css('display', 'none');
-    if (validForId == '') {
-      return;
-    }
-    var url = constructurl(base, '?', validForId, validForVariant, validForAspect);
-    $.ajax(url, settings)
-      .done(finalResult.bind(finalResult, validForId, validForVariant, validForAspect));
-  }
-  function changeDetect() {
-    var nid = idelement.val(), nvariant = variantelement.val(), naspect = aspectelement.val();
-    var changed = false;
-    if (nid != id) {
-      id = nid;
-      changed = true;
-    }
-    if (nvariant != variant) {
-      variant = nvariant;
-      changed = true;
-    }
-    if (naspect != aspect) {
-      aspect = naspect;
-      changed = true;
-    }
-    if (changed) {
-      updateSearch(id, variant, aspect);
-    }
-  }
-  var idelement = $('#exploreid');
-  idelement.keyup(changeDetect);
-  idelement.change(changeDetect);
-  var variantelement = $('#explorevariant');
-  variantelement.keyup(changeDetect);
-  variantelement.change(changeDetect);
-  var aspectelement = $('#exploreaspect');
-  aspectelement.keyup(changeDetect);
-  aspectelement.change(changeDetect);
-  // check for #id= argument
-  function hashKey() {
-    var hash = document.location.hash;
-    function parseHash(name) {
-      var ind = hash.indexOf(name + '=');
-      if (ind == -1) {
-        return '';
-      }
-      var end = hash.indexOf('&', ind + name.length + 1);
-      if (end == -1) {
-        end = hash.length;
-      }
-      return hash.substring(ind + name.length + 1, end);
-    }
-    return {
-      id: parseHash('id'),
-      variant: parseHash('variant'),
-      aspect: parseHash('aspect'),
-      pcontent: parseHash('key')
-    };
-  }
-  var hash = hashKey();
-  pcontent = hash.pcontent;
-  variantelement.val(hash.variant);
-  aspectelement.val(hash.aspect);
-  idelement.val(hash.id);
-  idelement.change();
+    $('#exploreresult').on('dblclick', 'pre', function (e) {
+        var pre = $('#exploreresult pre'), data;
+        if (!pre.attr('contenteditable')) {
+            data = JSON.parse(pre.text());
+            delete data.id;
+            delete data.version;
+            delete data.meta;
+            $('#exploreresult').html('<pre contenteditable="true"><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
+            $('#exploreresult pre code').each(function (i, e) { hljs.highlightBlock(e); });
+            $('#explorefound').css('display', 'none');
+            $('#editsave').css('display', 'block');
+        }
+    });
+    $('#plink').on('click', function (e) {
+        window.document.location = constructurl('index.html', '#', id, variant, aspect, pcontent);
+        e.preventDefault();
+    });
+    $('#editsave').on('click', function (e) {
+        var data = $('#exploreresult pre').text(),
+            url = constructurl(base, '?', id, variant, aspect);
+        $.ajax(url, $.extend({}, { data: data, contentType: 'application/json', method: 'PUT' }))
+            .done(function (result) {
+                if (result.id) {
+                    id = result.id;
+                    $('#exploreid').val(id);
+                }
+                finalResult(id, variant, aspect, result);
+            });
+    });
+    idelement.keyup(changeDetect);
+    idelement.change(changeDetect);
+    variantelement.keyup(changeDetect);
+    variantelement.change(changeDetect);
+    aspectelement.keyup(changeDetect);
+    aspectelement.change(changeDetect);
+    hash = hashKey();
+    pcontent = hash.pcontent;
+    variantelement.val(hash.variant);
+    aspectelement.val(hash.aspect);
+    idelement.val(hash.id);
+    idelement.change();
 }
--- src/main/webapp/js/explore.js
+++ src/main/webapp/js/explore.js
@@ -1,205 +1,204 @@
-var explore = function(window, $) {
-  var base = 'cb/', id = '', dcontent = '', dvariant = '', daspect = '', settings = {
-    cache: false,
-    dataType: 'json'
-  };
-  $('#exploreresult').on('click', '.link', function(e) {
-    if (e.ctrlKey) {
-      return;
-    }
-    $('#exploreid').val($(e.target).text());
-    $('#exploreid').change();
-    e.preventDefault();
-  });
-  $('#dlink').on('click', function(e) {
-    document.location = constructurl('data.html', '#', id, dcontent, dvariant, daspect);
-    e.preventDefault();
-  });
-  function constructurl(url, quest, id, dcontent, dvariant, daspect) {
-    if (id) {
-      url = url + quest + 'key=' + id;
-      quest = '&';
-    }
-    if (dvariant) {
-      url = url + quest + 'variant=' + dvariant;
-      quest = '&';
-    }
-    if (daspect) {
-      url = url + quest + 'aspect=' + daspect;
-    }
-    if (dcontent) {
-      url = url + quest + 'id=' + dcontent;
-    }
-    return url;
-  }
-  $('.createview').on('click', function(e) {
-    $.ajax(base + 'view', $.extend({}, {method: 'POST', data: '{"operation":"create"}', contentType: 'application/json' }, settings))
-      .done(function() {
-        $('#explorelist').css('display', 'none');
-      })
-      .fail(function() {
-        console.log('Create ids view failed');
-      });
-    e.preventDefault();
-  });
-  
-  function finalResult(validForId, data) {
-    if (validForId != id) {
-      return;
-    }
-    $('#explorefound').css('display', 'block');
-    $('#exploreresult').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
-    $('#exploreresult pre code').each(function(i, e) { hljs.highlightBlock(e); });
-    var url = document.location.href;
-    if (url.indexOf('#') != -1) {
-      url = url.substring(0, url.indexOf('#'));
-    }
-    document.location = constructurl(url, '#', validForId, dcontent, dvariant, daspect);
-  }
-  function expandResult(validForId, dataType, topData, currentData, resultKey, count, resultData) {
-    if (validForId != id) {
-      return;
-    }
-    var newData = resultData.data;
-    if (dataType == null) {
-      dataType = typeFromId(resultData.id);
-    }
-    newData = $.extend({}, {'_InternalId': resultData.id}, newData);
-    if (resultKey) {
-      currentData[resultKey] = newData;
-    } else {
-      topData = newData;
-    }
-    if (dataType == 'HangerId') {
-      var get = 'HangerVersion::' + newData.key;
-      $.ajax(base + get, settings)
-        .done(expandResult.bind(expandResult, validForId, 'HangerVersion', topData, newData, 'key :  ' + newData.key, count))
-        .fail(expandError.bind(expandError, validForId, topData, newData, 'key-' + newData.key, count));
-    } else if (dataType == 'HangerVersion') {
-      var get = 'Hanger::' + newData.contentId.key + "::" + newData.version;
-      $.ajax(base + get, settings)
-        .done(expandResult.bind(expandResult, validForId, 'Hanger', topData, newData, 'version :  ' + newData.version, count))
-        .fail(expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version, count));
-    } else if (dataType == 'Hanger') {
-      var aspects = { count: 0 };
-      for (var i in newData.aspectLocations) {
-        aspects.count = aspects.count + 1;
-      }
-      for (var i in newData.aspectLocations) {
-        var aspect = newData.aspectLocations[i];
-        var get = 'AspectVersion::' + aspect.key;
-        $.ajax(base + get, settings)
-          .done(expandResult.bind(expandResult, validForId, 'AspectVersion', topData, aspect, 'key :  ' + aspect.key, count))
-          .fail(expandError.bind(expandError, validForId, topData, aspect, 'key :  ' + aspect.key, count));
-      }
-    } else if (dataType == 'AspectVersion') {
-      var get = 'Aspect::' + newData.contentId.key + '::' + newData.version;
-      $.ajax(base + get, settings)
-        .done(expandResult.bind(expandResult, validForId, 'Aspect', topData, newData, 'version : ' + newData.version, count))
-        .fail(expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version, count));
-    } else {
-      if (count) {
-        count.count = count.count - 1;
-        if (count.count == 0) {
-          finalResult(validForId, topData);
+function explore(window, $, hljs) {
+    'use strict';
+    var base = 'cb/', id = '', dcontent = '', dvariant = '', daspect = '', idelement, hash,
+        settings = { cache: false, dataType: 'json' };
+    $('#exploreresult').on('click', '.link', function (e) {
+        if (e.ctrlKey) {
+            return;
         }
-      } else {
-        finalResult(validForId, topData);
-      }
-    }
-  }
-  function expandError(validForId, topData, currentData, resultKey, count, jqXHR, textStatus) {
-    if (validForId != id) {
-      return;
-    }
-    currentData[resultKey] = 'not found';
-    if (count) {
-      count.count = count.count - 1;
-      if (count.count == 0) {
-        finalResult(validForId, topData);
-      }
-    } else {
-      finalResult(validForId, topData);
-    }
-  }
-  function search(searchFor) {
-    if (searchFor != id) {
-      return;
-    }
-    $('#explorefound').css('display', 'none');
-    var searchUrl = base + 'search/ids?key=' + searchFor + '&key=HangerId::' + searchFor + '&key=HangerVersion::' + searchFor + '&key=AspectVersion::' + searchFor;
-    $.ajax(searchUrl, settings)
-      .done(function(found) {
-        if (searchFor != id) {
-          return;
+        $('#exploreid').val($(e.target).text());
+        $('#exploreid').change();
+        e.preventDefault();
+    });
+    function constructurl(url, quest, id, dcontent, dvariant, daspect) {
+        if (id) {
+            url = url + quest + 'key=' + id;
+            quest = '&';
         }
-        if (found.length == 1) {
-          $('#exploreid').val(found[0]);
-          $('#exploreid').change();
-        } else if (found.length > 0) {
-          $('#exploreresult').css('display', 'block');
-          $('#exploreresult').html('<ul>' + found.map(function(id) { return '<li><a href="#key=' + id + '" class="link">' + id + '</a></li>'; }).join('\n') + '</ul>');
+        if (dvariant) {
+            url = url + quest + 'variant=' + dvariant;
+            quest = '&';
         }
-      })
-      .fail(function() {
-        $('#explorelist').css('display', 'block');
-      }
-    );
-  }
-  function typeFromId(id) {
-    if (id.indexOf('::') == -1) {
-      return 'unknown';
+        if (daspect) {
+            url = url + quest + 'aspect=' + daspect;
+        }
+        if (dcontent) {
+            url = url + quest + 'id=' + dcontent;
+        }
+        return url;
     }
-    return id.substring(0, id.indexOf('::'));
-  }
-  function updateSearch(searchFor) {
-    if (searchFor != id) {
-      return;
+    $('#dlink').on('click', function (e) {
+        window.document.location = constructurl('data.html', '#', id, dcontent, dvariant, daspect);
+        e.preventDefault();
+    });
+    $('.createview').on('click', function (e) {
+        $.ajax(base + 'view', $.extend({}, {method: 'POST', data: '{"operation":"create"}', contentType: 'application/json' }, settings))
+            .done(function () {
+                $('#explorelist').css('display', 'none');
+            })
+            .fail(function () {
+                window.console.log('Create ids view failed');
+            });
+        e.preventDefault();
+    });
+    function finalResult(validForId, data) {
+        if (validForId !== id) {
+            return;
+        }
+        $('#explorefound').css('display', 'block');
+        $('#exploreresult').html('<pre><code>' + JSON.stringify(data, null, "  ") + '</code></pre>');
+        $('#exploreresult pre code').each(function (i, e) { hljs.highlightBlock(e); });
+        var url = window.document.location.href;
+        if (url.indexOf('#') !== -1) {
+            url = url.substring(0, url.indexOf('#'));
+        }
+        window.document.location = constructurl(url, '#', validForId, dcontent, dvariant, daspect);
     }
-    $('#explorefound').css('display', 'none');
-    var findUrl = base + searchFor;
-    $.ajax(findUrl, settings)
-      .done(expandResult.bind(expandResult, searchFor, null, null, null, null, null))
-      .fail(search.bind(search, searchFor));
-  }
-  function changeDetect() {
-    var nid = idelement.val();
-    if (nid != id) {
-      id = nid;
-      updateSearch(id);
+    function typeFromId(id) {
+        if (id.indexOf('::') === -1) {
+            return 'unknown';
+        }
+        return id.substring(0, id.indexOf('::'));
     }
-  }
-  var idelement = $('#exploreid');
-  idelement.keyup(changeDetect);
-  idelement.change(changeDetect);
-  $('#exploreid').keydown(function() {
-    var now = $('#exploreid')
-  });
-  // check for #key= argument
-  // check for #id= argument
-  function hashKey() {
-    var hash = document.location.hash;
-    function parseHash(name) {
-      var ind = hash.indexOf(name + '=');
-      if (ind == -1) {
-        return '';
-      }
-      var end = hash.indexOf('&', ind + name.length + 1);
-      if (end == -1) {
-        end = hash.length;
-      }
-      return hash.substring(ind + name.length + 1, end);
+    function expandError(validForId, topData, currentData, resultKey, count, jqXHR, textStatus) {
+        if (validForId !== id) {
+            return;
+        }
+        currentData[resultKey] = 'not found';
+        if (count) {
+            count.count = count.count - 1;
+            if (count.count === 0) {
+                finalResult(validForId, topData);
+            }
+        } else {
+            finalResult(validForId, topData);
+        }
     }
-    return {
-      id: parseHash('key'),
-      dvariant: parseHash('variant'),
-      daspect: parseHash('aspect'),
-      dcontent: parseHash('id')
-    };
-  }
-  var hash = hashKey();
-  dcontent = hash.dcontent;
-  dvariant = hash.dvariant;
-  daspect = hash.daspect;
-  $('#exploreid').val(hash.id).change();
+    function expandResult(validForId, dataType, topData, currentData, resultKey, count, resultData) {
+        var newData = resultData.data, get, aspects, aspect, i;
+        if (validForId !== id) {
+            return;
+        }
+        if (dataType === null) {
+            dataType = typeFromId(resultData.id);
+        }
+        newData = $.extend({}, {'_InternalId': resultData.id}, newData);
+        if (resultKey) {
+            currentData[resultKey] = newData;
+        } else {
+            topData = newData;
+        }
+        if (dataType === 'HangerId') {
+            get = 'HangerVersion::' + newData.key;
+            $.ajax(base + get, settings)
+                .done(expandResult.bind(expandResult, validForId, 'HangerVersion', topData, newData, 'key :  ' + newData.key, count))
+                .fail(expandError.bind(expandError, validForId, topData, newData, 'key-' + newData.key, count));
+        } else if (dataType === 'HangerVersion') {
+            get = 'Hanger::' + newData.contentId.key + "::" + newData.version;
+            $.ajax(base + get, settings)
+                .done(expandResult.bind(expandResult, validForId, 'Hanger', topData, newData, 'version :  ' + newData.version, count))
+                .fail(expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version, count));
+        } else if (dataType === 'Hanger') {
+            aspects = { count: 0 };
+            for (i in newData.aspectLocations) {
+                if (newData.aspectLocations.hasOwnProperty(i)) {
+                    aspects.count = aspects.count + 1;
+                }
+            }
+            for (i in newData.aspectLocations) {
+                if (newData.aspectLocations.hasOwnProperty(i)) {
+                    aspect = newData.aspectLocations[i];
+                    get = 'AspectVersion::' + aspect.key;
+                    $.ajax(base + get, settings)
+                        .done(expandResult.bind(expandResult, validForId, 'AspectVersion', topData, aspect, 'key :  ' + aspect.key, count))
+                        .fail(expandError.bind(expandError, validForId, topData, aspect, 'key :  ' + aspect.key, count));
+                }
+            }
+        } else if (dataType === 'AspectVersion') {
+            get = 'Aspect::' + newData.contentId.key + '::' + newData.version;
+            $.ajax(base + get, settings)
+                .done(expandResult.bind(expandResult, validForId, 'Aspect', topData, newData, 'version : ' + newData.version, count))
+                .fail(expandError.bind(expandError, validForId, topData, newData, 'version-' + newData.version, count));
+        } else {
+            if (count) {
+                count.count = count.count - 1;
+                if (count.count === 0) {
+                    finalResult(validForId, topData);
+                }
+            } else {
+                finalResult(validForId, topData);
+            }
+        }
+    }
+    function search(searchFor) {
+        if (searchFor !== id) {
+            return;
+        }
+        $('#explorefound').css('display', 'none');
+        var searchUrl = base + 'search/ids?key=' + searchFor + '&key=HangerId::' + searchFor + '&key=HangerVersion::' + searchFor + '&key=AspectVersion::' + searchFor;
+        $.ajax(searchUrl, settings)
+            .done(function (found) {
+                if (searchFor !== id) {
+                    return;
+                }
+                if (found.length === 1) {
+                    $('#exploreid').val(found[0]);
+                    $('#exploreid').change();
+                } else if (found.length > 0) {
+                    $('#exploreresult').css('display', 'block');
+                    $('#exploreresult').html('<ul>' + found.map(function (id) { return '<li><a href="#key=' + id + '" class="link">' + id + '</a></li>'; }).join('\n') + '</ul>');
+                }
+            })
+            .fail(function () {
+                $('#explorelist').css('display', 'block');
+            });
+    }
+    function updateSearch(searchFor) {
+        if (searchFor !== id) {
+            return;
+        }
+        $('#explorefound').css('display', 'none');
+        var findUrl = base + searchFor;
+        $.ajax(findUrl, settings)
+            .done(expandResult.bind(expandResult, searchFor, null, null, null, null, null))
+            .fail(search.bind(search, searchFor));
+    }
+    function changeDetect() {
+        var nid = idelement.val();
+        if (nid !== id) {
+            id = nid;
+            updateSearch(id);
+        }
+    }
+    idelement = $('#exploreid');
+    idelement.keyup(changeDetect);
+    idelement.change(changeDetect);
+    $('#exploreid').keydown(function () {
+        var now = $('#exploreid');
+    });
+    function hashKey() {
+        var hash = window.document.location.hash;
+        function parseHash(name) {
+            var ind = hash.indexOf(name + '='), end;
+            if (ind === -1) {
+                return '';
+            }
+            end = hash.indexOf('&', ind + name.length + 1);
+            if (end === -1) {
+                end = hash.length;
+            }
+            return hash.substring(ind + name.length + 1, end);
+        }
+        return {
+            id: parseHash('key'),
+            dvariant: parseHash('variant'),
+            daspect: parseHash('aspect'),
+            dcontent: parseHash('id')
+        };
+    }
+    hash = hashKey();
+    dcontent = hash.dcontent;
+    dvariant = hash.dvariant;
+    daspect = hash.daspect;
+    $('#exploreid').val(hash.id).change();
 }
